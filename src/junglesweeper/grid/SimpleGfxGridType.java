afb187715af0b964c19f16ab1369453e26c1ae60
fabio fernandes <fabio.garciafernandes@gmail.com>
2017-05-30 21:59:59 +0100

    seems that everything is almost in order...

---
+++ src/junglesweeper/grid/SimpleGfxGridType.java
@@ -0,0 +1,55 @@
+package junglesweeper.grid;
+
+/**
+ * Created by codecadet on 30/05/17.
+ */
+public enum SimpleGfxGridType {
+    MAP_GRID("./assets/pictures/grid.png", 1, 190, 35, 17, 17, 40),
+    INFO_GRID("./assets/pictures/sensor.png", 2, 32, 35, -1, -1, 1);
+
+    private int priority;
+    private String imagePath;
+    private int xPadding;
+    private int yPadding;
+    private int cols;
+    private int rows;
+    private int cellSize;
+
+    SimpleGfxGridType(String imagePath, int priority, int xPadding, int yPadding, int cols, int rows, int cellSize) {
+        this.priority= priority;
+        this.imagePath = imagePath;
+        this.xPadding = xPadding;
+        this.yPadding = yPadding;
+        this.cols = cols;
+        this.rows = rows;
+        this.cellSize = cellSize;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public int getxPadding() {
+        return xPadding;
+    }
+
+    public int getyPadding() {
+        return yPadding;
+    }
+
+    public int getCellSize() {
+        return cellSize;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+}
